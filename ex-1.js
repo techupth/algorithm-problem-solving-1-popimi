3e4c90e84455b9d96136af8c0a0916ce0d0a0ad4
Pop <shounen.pop@gmail.com>
2024-07-10 13:10:33 +0700

    feat: add answer

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,18 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  const result = [];
+  //   for(let num of numbers){
+  //       for(let n of numbers){
+  //           if(num + n === target) {
+  //               result.push(numbers.indexOf(num))
+  //           }
+  //       }
+  //   }
+  numbers.forEach((num) =>
+    numbers.forEach((n) =>
+      num + n === target ? result.push(numbers.indexOf(num)) : null
+    )
+  );
+  return result;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
